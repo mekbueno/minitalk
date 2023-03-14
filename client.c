13ebc7ac99eab60c38f80a4d499d90a037671de2
mekbueno <mek.bueno@gmail.com>
2023-03-14 21:04:12 +0100

    refactored ft_atoi - isspace function not necessary

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: mbueno <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:23:14 by mbueno            #+#    #+#             */
-/*   Updated: 2023/03/14 14:51:26 by mbueno           ###   ########.fr       */
+/*   Updated: 2023/03/14 21:03:30 by mbueno           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,46 +16,36 @@
 #include <stdio.h>
 #include "ft_printf/ft_printf.h"
 
-
-static int	ft_isspace(char c)
+int ft_atoi(const char *str)
 {
-	if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || \
-			c == '\r' || c == '\f')
-		return (1);
-	else
-		return (0);
-}
+	long num;
+	long sign;
+	int i;
 
-int	ft_atoi(const char *nptr)
-{
-	long	nbr;
-	long	sign;
-	int		i;
-
-	nbr = 0;
+	num = 0;
 	sign = 1;
 	i = 0;
-	while (ft_isspace(nptr[i]) && nptr[i] != '\0')
+	while (str[i] != '\0' && str[i] == str[i] >= 32 && str[i] == 127)
 		i++;
-	if (nptr[i] == '-')
+	if (str[i] == '-')
 		sign = -1;
-	if (nptr[i] == '+' || nptr[i] == '-')
+	if (str[i] == '+' || str[i] == '-')
 		i++;
-	while (nptr[i] != '\0' && ('0' <= nptr[i] && nptr[i] <= '9'))
+	while (str[i] != '\0' && ('0' <= str[i] && str[i] <= '9'))
 	{
-		nbr = (nbr * 10) + (nptr[i] - '0');
-		if (nbr > 2147483647 && sign == 1)
+		num = (num * 10) + (str[i] - '0');
+		if (num > 2147483647 && sign == 1)
 			return (-1);
-		if (nbr > 2147483648 && sign == -1)
+		if (num > 2147483648 && sign == -1)
 			return (0);
 		i++;
 	}
-	return (sign * nbr);
+	return (sign * num);
 }
 
-void	ft_send(int pid, int c)
+void ft_send(int pid, int c)
 {
-	int	bit;
+	int bit;
 
 	bit = 0;
 	while (bit < 8)
@@ -64,15 +54,15 @@ void	ft_send(int pid, int c)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(5);
+		usleep(5000);
 		bit++;
 	}
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	int	pid;
-	int	i;
+	int pid;
+	int i;
 
 	i = 0;
 	if (ac != 3)
