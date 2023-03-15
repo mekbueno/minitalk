d32e45a7a9d8c3b6730cab48a91441d851bc3262
michael bueno <103365720+mekbueno@users.noreply.github.com>
2023-03-15 12:06:10 +0100

    Update client.c
    found typo in while cond.

--- client.c
+++ client.c
@@ -25,7 +25,7 @@ int ft_atoi(const char *str)
 	num = 0;
 	sign = 1;
 	i = 0;
-	while (str[i] != '\0' && str[i] == str[i] >= 32 && str[i] == 127)
+	while (str[i] != '\0' && str[i] >= 32 && str[i] == 127)
 		i++;
 	if (str[i] == '-')
 		sign = -1;
